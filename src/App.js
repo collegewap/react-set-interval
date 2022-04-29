61fd93fcf6271d30b804e5de8688d4aa89892d30
Abhishek EH <collegewap@gmail.com>
2022-04-29 19:26:27 +0530

    counter example

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import { useState } from "react";
 import { useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
   useEffect(() => {
     const intervalId = setInterval(() => {
-      console.log("This message will appear after each second");
+      setCounter((oldValue) => oldValue + 1);
     }, 1000);
     return () => {
+      console.log("clearing interval");
       clearInterval(intervalId);
     };
   }, []);
 
-  return <div className="App"></div>;
+  return <div className="App">{counter}</div>;
 }
 
 export default App;
